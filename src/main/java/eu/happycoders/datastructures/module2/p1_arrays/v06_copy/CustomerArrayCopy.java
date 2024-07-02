ac86476e2c81efeada9485ff3b87c82839792b0f
Sven Woltmann <sven@happycoders.eu>
2024-07-02 09:22:11 +0200

    Add code for module 1, part 1, video 6

---
+++ src/main/java/eu/happycoders/datastructures/module2/p1_arrays/v06_copy/CustomerArrayCopy.java
@@ -0,0 +1,11 @@
+import eu.happycoders.datastructures.module2.common.Customer;
+
+void main() {
+  Customer[] array = {
+      new Customer("jmarsh"),
+      new Customer("lgibson"),
+      new Customer("award"),
+  };
+
+
+}
