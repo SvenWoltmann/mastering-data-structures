bb288d19f08a96219be637e35b51c1efd2939b13
Sven Woltmann <sven@happycoders.eu>
2024-07-12 14:44:55 +0200

    Add code for module 1, part 2, video 8

---
+++ src/main/java/eu/happycoders/datastructures/module2/p2_lists/v08_singleton_lists/SingletonListBenchmark.java
@@ -0,0 +1,24 @@
+package eu.happycoders.datastructures.module2.p2_lists.v08_singleton_lists;
+
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+
+@Warmup(iterations = 2, time = 5)
+@Measurement(iterations = 5, time = 5)
+@Fork(1)
+public class SingletonListBenchmark {
+
+
+}
