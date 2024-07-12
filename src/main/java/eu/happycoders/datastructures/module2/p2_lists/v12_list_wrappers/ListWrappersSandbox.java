7a85b6e43679378b629eba628639895b52df7487
Sven Woltmann <sven@happycoders.eu>
2024-07-12 15:56:52 +0200

    Add code for module 1, part 2, video 12

---
+++ src/main/java/eu/happycoders/datastructures/module2/p2_lists/v12_list_wrappers/ListWrappersSandbox.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+void main() {
+  var fruits = new ArrayList<>(Arrays.asList("apple", "banana", "clementine", "date", "eggplant"));
+  System.out.println("fruits = " + fruits);
+
+
+}
