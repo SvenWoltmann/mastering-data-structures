a96dfdad67c983b13fb1fd99c09a7d8b457ce5e8
Sven Woltmann <sven@happycoders.eu>
2024-07-13 19:10:44 +0200

    Add code for module 1, part 4, video 4

---
+++ src/main/java/eu/happycoders/datastructures/module2/p4_searching_sorting/v04_seq_search_lists/CustomerListSequentialSearch.java
@@ -0,0 +1,14 @@
+import eu.happycoders.datastructures.module2.p4_searching_sorting.common.Customer;
+
+import java.util.List;
+
+void main() {
+  var list = List.of(
+      new Customer("jmarsh", 97221, "Jordan", "Marsh"),
+      new Customer("lgibson", 17346, "Lane", "Gibson"),
+      new Customer("award", 39632, "Aiden", "Ward"),
+      new Customer("agibson", 36376, "Ashton", "Gibson")
+  );
+
+
+}
