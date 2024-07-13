12599fa74d8a976d8a8fe54ce0420f93508f890b
Sven Woltmann <sven@happycoders.eu>
2024-07-13 13:50:37 +0200

    Add code for module 1, part 4, video 2

---
+++ src/main/java/eu/happycoders/datastructures/module2/p4_searching_sorting/v02_object_arrays/SortingCustomers.java
@@ -0,0 +1,11 @@
+import eu.happycoders.datastructures.module2.p4_searching_sorting.v02_object_arrays.Customer;
+
+void main() {
+  Customer[] customers =
+      new Customer[]{
+          new Customer("jmarsh"), // 97221, "Jordan", "Marsh"
+          new Customer("lgibson"), // 17346, "Lane", "Gibson"
+          new Customer("award") // 39632, "Aiden", "Ward"
+      };
+
+}
