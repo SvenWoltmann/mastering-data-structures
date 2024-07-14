dfbfedefee00c349af1bb34d86dec77793742cbc
Sven Woltmann <sven@happycoders.eu>
2024-07-14 16:46:53 +0200

    Add code for module 1, part 4, video 10

---
+++ src/main/java/eu/happycoders/datastructures/module2/p4_searching_sorting/v10_binary_search_lists/BinarySearchStringList.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+void main() {
+  var fruits = List.of("apricot", "avocado", "banana", "blueberry", "boysenberry",
+      "cantaloupe", "cherry", "dragon fruit", "fig", "grape", "grapefruit", "guava", "honeydew",
+      "huckleberry", "jujube", "kiwi", "kumquat", "lime", "mango", "peach", "pineapple",
+      "pomegranate", "raspberry", "satsuma", "star fruit", "strawberry", "tomato", "watermelon");
+
+
+}
